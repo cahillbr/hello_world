a9806f25f5d66dae2f0d71b800e3c1f7ffdfae96
cahillbr <cahillbr@oregonstate.edu>
2023-01-18 22:38:57 -0800

    w

---
+++ hello_gradescope.py
@@ -0,0 +1,10 @@
+# Name:Brendan Cahill
+# OSU Email: cahillbr@oregonstate.edu
+# Course: CS261 - Data Structures
+# Assignment: Assignment 0
+# Due Date: 01/18/23
+# Description: Test gradescope by writing a hello world assignment.
+#
+
+def gradescope_test():
+    return 42
